9d83bace7b88d0387b3ed2dac953c63a22298960
Jonathan Payne <paynejd@gmail.com>
2019-06-04 10:42:11 -0400

    Added initial set of OCL constants

--- ocldev/oclconstants.py
+++ ocldev/oclconstants.py
@@ -5,4 +5,24 @@ Shared constants used throughout the ocldev package
 
 class OclConstants:
     """ Shared constants used in the ocldev package """
-    pass
+
+    # OCL resource type constants
+    RESOURCE_TYPE_USER = 'User'
+    RESOURCE_TYPE_ORGANIZATION = 'Organization'
+    RESOURCE_TYPE_SOURCE = 'Source'
+    RESOURCE_TYPE_COLLECTION = 'Collection'
+    RESOURCE_TYPE_CONCEPT = 'Concept'
+    RESOURCE_TYPE_MAPPING = 'Mapping'
+    RESOURCE_TYPE_CONCEPT_REF = 'Concept_Ref'
+    RESOURCE_TYPE_MAPPING_REF = 'Mapping_Ref'
+    RESOURCE_TYPE_REFERENCE = 'Reference'
+    RESOURCE_TYPE_SOURCE_VERSION = 'Source Version'
+    RESOURCE_TYPE_COLLECTION_VERSION = 'Collection Version'
+
+    # API endpoint stems for owners
+    OWNER_STEM_USERS = 'users'
+    OWNER_STEM_ORGS = 'orgs'
+
+    # API endpoint stems for repositories
+    REPO_STEM_SOURCES = 'sources'
+    REPO_STEM_COLLECTIONS = 'collections'
