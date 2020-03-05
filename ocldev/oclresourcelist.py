6a874f0bc7961d6a09dd4dbfc442830c1aabb3b1
Jonathan Payne <paynejd@gmail.com>
2020-03-05 11:36:22 -0500

    Initial commit of OclResourceList and OclValidator objects

---
+++ ocldev/oclresourcelist.py
@@ -0,0 +1,97 @@
+""" Classes to manage a list of OCL resources """
+import json
+import csv
+import sys
+import oclvalidator
+
+
+class OclResourceList(object):
+    """ Generic class to manage a list of OCL resources """
+
+    def __init__(self, resources=None):
+        """ Initialize the OclResourceList instance """
+        self._resources = []
+        self._current_iter = 0
+        if resources:
+            self.load_resources(resources)
+
+    def load_resources(self, resources):
+        """ Load resource list """
+        self._resources = resources
+
+    def __iter__(self):
+        """ Iterator for the OclResourceList class """
+        self._current_iter = 0
+        return self
+
+    def __len__(self):
+        """ Number of resources in this list """
+        return len(self._resources)
+
+    def next(self):
+        """ Get the next item in the list """
+        if self._current_iter >= len(self._resources):
+            raise StopIteration
+        else:
+            self._current_iter += 1
+            return self.get(self._current_iter - 1)
+
+    def append(self, resources):
+        """ Add one resource or a list of resources to the list """
+        if not isinstance(resources, list):
+            resources = [resources]
+        for resource in resources:
+            if not isinstance(resource, dict):
+                raise TypeError("Cannot append resource of type '%s'" % type(resource))
+            self._resources.append(resource)
+
+    def __getitem__(self, index):
+        """ Get an item from the list """
+        return self._resources[index]
+
+    def display_as_json(self):
+        """ Display the resource list as JSON """
+        for resource in self._resources:
+            print json.dumps(resource)
+
+    def display_as_csv(self):
+        """ Display the resource list as CSV """
+        output_stream = sys.stdout
+        columns = self.get_unique_column_headers()
+        writer = csv.DictWriter(output_stream, fieldnames=columns)
+        writer.writeheader()
+        for resource in self._resources:
+            writer.writerow(resource)
+
+    def get_unique_column_headers(self):
+        """ Get a list of unique column headers in the resource list """
+        columns = ['resource_type', 'owner', 'id']
+        for resource in self._resources:
+            for key in resource:
+                if key not in columns:
+                    columns.append(key)
+        return columns
+
+
+class OclCsvResourceList(OclResourceList):
+    """ Generic class to manage a list of OCL resources """
+
+    def __init__(self, resources=None):
+        """ Initialize the OclCsvResourceList instance """
+        OclResourceList.__init__(self, resources=resources)
+
+    def validate(self):
+        """ Validate the resource list using the OclCsvValidator """
+        oclvalidator.OclCsvValidator.validate(self)
+
+
+class OclJsonResourceList(OclResourceList):
+    """ Generic class to manage a list of OCL resources """
+
+    def __init__(self, resources=None):
+        """ Initialize the OclJsonResourceList instance """
+        OclResourceList.__init__(self, resources=resources)
+
+    def validate(self):
+        """ Validate the resource list using the OclCsvValidator """
+        oclvalidator.OclJsonValidator.validate(self)
---
+++ ocldev/oclvalidator.py
@@ -0,0 +1,124 @@
+"""
+Classes to validate JSON or CSV resource definitions
+"""
+import jsonschema
+import oclresourcelist
+
+
+class OclJsonValidator(object):
+    """ Class to validate OCL-formatted JSON resource definitions """
+
+    @staticmethod
+    def validate(resources):
+        """ Validate list of resources """
+        if isinstance(resources, dict):
+            resources = [resources]
+        if isinstance(resources, list) and isinstance(resources, oclresourcelist.OclResourceList):
+            for resource in resources:
+                OclJsonValidator.validate_resource(resource)
+        else:
+            raise TypeError("Expected OclResourceList, list of resources, or a single resource. '%s' given." % str(
+                type(resources)))
+
+    @staticmethod
+    def validate_resource(resource, resource_type=''):
+        """ Validate resource against schema """
+        if not resource_type:
+            if 'resource_type' in resource:
+                resource_type = resource['resource_type']
+        if not resource_type:
+            raise Exception("Must provide 'resource_type' as a resource attribute or "
+                            "specify as an argument. Neither provided.")
+        elif resource_type not in OclJsonValidator.VALIDATION_SCHEMAS:
+            raise Exception("Unrecognized resource type '%s'" % resource_type)
+        jsonschema.validate(
+            instance=resource, schema=OclJsonValidator.VALIDATION_SCHEMAS[resource_type])
+
+    # Validation schemas
+    VALIDATION_SCHEMA_CONCEPT = {}
+    VALIDATION_SCHEMA_MAPPING = {}
+    VALIDATION_SCHEMA_REFERENCE = {}
+    VALIDATION_SCHEMA_REPOSITORY_VERSION = {}
+    VALIDATION_SCHEMA_SOURCE = {}
+    VALIDATION_SCHEMA_COLLECTION = {}
+    VALIDATION_SCHEMA_ORGANIZATION = {}
+    VALIDATION_SCHEMAS = {
+        'Concept': VALIDATION_SCHEMA_CONCEPT,
+        'Mapping': VALIDATION_SCHEMA_MAPPING,
+    }
+
+class OclCsvValidator(object):
+    """ Class to validate OCL-formatted CSV resource definitions """
+
+    @staticmethod
+    def validate(resources):
+        """ Validate list of resources """
+        if isinstance(resources, dict):
+            resources = [resources]
+        if isinstance(resources, list) and isinstance(resources, oclresourcelist.OclResourceList):
+            for resource in resources:
+                OclCsvValidator.validate_resource(resource)
+        else:
+            raise TypeError("Expected OclResourceList, list of resources, or a single resource. '%s' given." % str(
+                type(resources)))
+
+    @staticmethod
+    def validate_resource(resource, resource_type=''):
+        """ Validate resource against schema """
+        if not resource_type:
+            if 'resource_type' in resource:
+                resource_type = resource['resource_type']
+        if not resource_type:
+            raise Exception("Must provide 'resource_type' as a resource attribute or specify "
+                            "as an argument. Neither provided.")
+        elif resource_type not in OclCsvValidator.VALIDATION_SCHEMAS:
+            raise Exception("Unrecognized resource type '%s'" % resource_type)
+        jsonschema.validate(
+            instance=resource, schema=OclCsvValidator.VALIDATION_SCHEMAS[resource_type])
+
+    # Validation schemas
+    VALIDATION_SCHEMA_CONCEPT = {
+        "$schema": "http://json-schema.org/draft-07/schema#",
+        "$id": "http://openconceptlab.org/csv_concept.schema.json",
+        "title": "CSV_Concept",
+        "description": "A CSV-based OCL concept",
+        "type": "object",
+        "properties": {
+            "resource_type": {
+                "description": "",
+                "type": "string"
+            },
+            "owner_id": {
+                "description": "ID of the owner of this concept",
+                "type": "string"
+            },
+            "owner_type": {
+                "description": ("Resource type of the owner of this concept, "
+                                "either an Organization or User"),
+                "type": "string"
+            },
+            "source": {
+                "description": "OCL source",
+                "type": "string"
+            },
+            "id": {
+                "description": "ID of the concept",
+                "type": "string"
+            },
+            "name": {
+                "description": "Primary name of the concept",
+                "type": "string"
+            }
+        },
+        "required": ["resource_type", "id", "owner", "source", "concept_class"]
+    }
+    VALIDATION_SCHEMA_MAPPING = {}
+    VALIDATION_SCHEMA_REFERENCE = {}
+    VALIDATION_SCHEMA_REPOSITORY_VERSION = {}
+    VALIDATION_SCHEMA_SOURCE = {}
+    VALIDATION_SCHEMA_COLLECTION = {}
+    VALIDATION_SCHEMA_ORGANIZATION = {}
+    VALIDATION_SCHEMAS = {
+        'Concept': VALIDATION_SCHEMA_CONCEPT,
+        'Mapping': VALIDATION_SCHEMA_MAPPING,
+    }
