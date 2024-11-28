cb9b3769a24843620bbe630199f0b602c5caa647
Sny <sny.aggarwal@gmail.com>
2024-11-28 09:48:34 +0530

    OpenConceptLab/ocl_issues#2024 | bumping self to 0.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='ocldev',
-    version='0.2.1',
+    version='0.2.2',
     author='Open Concept Lab',
     author_email='info@openconceptlab.org',
     description='Python development library for working with OCL resources and APIs',
